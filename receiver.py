ac3bd14a45d3159b0dc35150e4de1d6ba1ac610a
arun <arun.sfc@outlook.com>
2024-09-30 15:55:41 -0700

    update receiver filtering of source ip addresses

--- receiver.py
+++ receiver.py
@@ -12,6 +12,11 @@ if __name__ == "__main__":
     parser.add_argument("dst_mac", type=str)
     parser.add_argument("iface", type=str)
     parser.add_argument("dst_port", type=int)
+    parser.add_argument("src_ip", type=str)
     args = parser.parse_args()
 
-    sniff(count=0, iface=args.iface, filter=f"udp and dst port {args.dst_port}", prn = lambda pkt: test_rtt_receiver(pkt, args.dst_port, args.dst_mac, args.iface), store=False)
+    sniff(count=0,
+          iface=args.iface,
+          filter=f"udp and dst port {args.dst_port} and src net {args.src_ip}",
+          prn = lambda pkt: test_rtt_receiver(pkt, args.dst_port, args.dst_mac, args.iface),
+          store=False)
